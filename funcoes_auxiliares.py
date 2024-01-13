a3281928ba67c643287066f09268855c1de082cf
Leo-9821 <79335147+Leo-9821@users.noreply.github.com>
2024-01-13 14:31:57 -0300

    Removendo funcoes_auxiliares.py

--- funcoes_auxiliares.py
+++
@@ -1,5 +0,0 @@
-from tkinter.filedialog import askdirectory
-
-
-def salva_arquivo(arquivo):
-    arq = asksaveasfile(initialfile=arquivo, defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
